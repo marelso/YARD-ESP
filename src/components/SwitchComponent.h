4d383292424d69af0fde389324ea8f0ae466f760
marelso <klein.marcelojr@gmail.com>
2025-04-07 21:09:09 -0300

    Including new class to handle switch components

---
+++ src/components/SwitchComponent.h
@@ -0,0 +1,41 @@
+#ifndef SWITCH_COMPONENT_H
+#define SWITCH_COMPONENT_H
+
+#include <Arduino.h>
+
+class SwitchComponent {
+    private:
+        int pin;
+
+        void turnOn() {
+            Serial.begin(115200);
+            Serial.printf("turning on pin %d \n", this->pin);
+
+            digitalWrite(this->pin, HIGH);
+        }
+
+        void turnOff() {
+            Serial.printf("Turning off pin %d \n", this->pin);
+            
+            digitalWrite(this->pin, LOW);
+        }
+
+    public:
+        SwitchComponent(int pin) {
+            this->pin = pin;
+            Serial.printf("Using pin %d \n", this->pin);
+
+            pinMode(this->pin, OUTPUT);
+            digitalWrite(this->pin, LOW);
+        }
+
+        void toggle() {
+            if(digitalRead(this->pin)) {
+                turnOff();
+            } else {
+                turnOn();
+            }
+        }
+};
+
+#endif
